e4cc99b79ad57b818f9fcdf35245675567cf3098
tranhoanglonghai2k <tranhoanglonghai2k@gmail.com>
2021-11-07 21:30:43 +0700

    Footer

--- frontend/src/pages/Footer/Footer.js
+++ frontend/src/pages/Footer/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Typography, Space } from "antd";
 
 function Footer() {
   const { Footer } = Layout;
+  const { Text } = Typography;
 
   return (
     <Layout>
       <Footer style={{ textAlign: "center" }}>
-        Ant Design ©2018 Created by Ant UED
+        <Space direction="vertical">
+          <Text strong>1. Đào Mạnh Thắng - MSSV: 20184193</Text>
+          <Text strong>2. Trần Thăng Đức - MSSV: 20184076</Text>
+          <Text strong>3. Trần Hoàng Long Hải - MSSV: 20184090</Text>
+        </Space>
       </Footer>
     </Layout>
   );
