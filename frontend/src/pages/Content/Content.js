a583d34a43ca2a07c9e1d960e6fbdb77be6b40a8
tranhoanglonghai2k <tranhoanglonghai2k@gmail.com>
2021-11-07 21:30:34 +0700

    fix Header

--- frontend/src/pages/Content/Content.js
+++ frontend/src/pages/Content/Content.js
@@ -11,11 +11,11 @@ function Content() {
         className="site-layout"
         style={{ padding: "0 50px", marginTop: 64 }}
       >
-        {/* <Breadcrumb style={{ margin: "16px 0" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb> */}
+        </Breadcrumb>
         <div
           className="site-layout-background"
           style={{ padding: 24, minHeight: 380 }}
--- frontend/src/pages/Header/Header.js
+++ frontend/src/pages/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Button } from "antd";
+import { HomeFilled, ZhihuSquareFilled } from "@ant-design/icons";
 import "./Header.css";
 
 function Header() {
@@ -11,51 +12,37 @@ function Header() {
         id="header"
         style={{ position: "fixed", zIndex: 1, width: "100%" }}
       >
-        <div className="logo">
-          <a href="#">
-            <img
-              src="https://jdictblog.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2020/05/18223843/cropped-logo-3.png"
-              alt="logo"
-              className="logo-dict"
-            ></img>
-          </a>
+        <div className="header-content-default">
+          <div className="logo">
+            <a href="#">
+              <img
+                src="https://jdictblog.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2020/05/18223843/cropped-logo-3.png"
+                alt="logo"
+                className="logo-dict"
+              ></img>
+            </a>
+          </div>
+
+          <Menu className="menu" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1" icon={<HomeFilled />}>
+              Tra Cứu
+            </Menu.Item>
+            <Menu.Item key="2" icon={<ZhihuSquareFilled />}>
+              Dịch
+            </Menu.Item>
+            <Menu.Item key="3">Chính tả</Menu.Item>
+            <Menu.Item key="3">Login</Menu.Item>
+            <Menu.Item key="3">Sign up</Menu.Item>
+          </Menu>
+
+          <div className="btn-default">
+            <Button>Login</Button>
+            <Button>Sign up</Button>
+          </div>
         </div>
-        <Menu className="menu" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">Tra Cứu</Menu.Item>
-          <Menu.Item key="2">Dịch</Menu.Item>
-          <Menu.Item key="3">Chính tả</Menu.Item>
-          <Menu.Item key="4">Login</Menu.Item>
-          <Menu.Item key="5">Sign up</Menu.Item>
-        </Menu>
       </Header>
     </Layout>
   );
 }
 
 export default Header;
-
-// (
-//   <div id="header">
-//     <ul id="nav" className="mgl-115  mgt-5">
-//       <li>
-{
-  /* <a href="#">
-  <img
-    src="https://jdictblog.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2020/05/18223843/cropped-logo-3.png"
-    alt="logo"
-    className="logo-dict"
-  ></img>
-  //{" "}
-</a>; */
-}
-//       </li>
-//       <li className="active">
-//         <a href="#">Dịch</a>
-//       </li>
-//     </ul>
-//     <div className="nav2 mgr-115 mgtb-20">
-//       <button className="btn login mgr-5 unactive">Login</button>
-//       <button className="btn sign-up unactive">Sign up</button>
-//     </div>
-//   </div>
-// )
