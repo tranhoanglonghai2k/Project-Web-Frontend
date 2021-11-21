dd8b4df90ad4d2af1e5a0ce9fd759ac29b4ada93
tranhoanglonghai2k <tranhoanglonghai2k@gmail.com>
2021-11-21 16:26:15 +0700

    Login page

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { Layout, Form, Input, Button, Checkbox } from "antd";
+import { Layout } from "antd";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 function Login() {
   const { Content } = Layout;
-  const onFinish = (values: any) => {
-    console.log("Success:", values);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
-  };
 
   return (
     <Layout>
@@ -21,49 +15,7 @@ function Login() {
           className="site-layout-background"
           style={{ padding: 24, minHeight: 2400, marginTop: 54 }}
         >
-          <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[
-                { required: true, message: "Please input your username!" },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
-
-            <Form.Item
-              name="remember"
-              valuePropName="checked"
-              wrapperCol={{ offset: 8, span: 16 }}
-            >
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+          <LoginForm />
         </div>
       </Content>
     </Layout>
