ea6fd16deb0e451adf8fa426b2e49c97a70aa449
tranhoanglonghai2k <tranhoanglonghai2k@gmail.com>
2021-11-21 21:13:03 +0700

    MyWord page

--- frontend/src/pages/MyWord/MyWord.js
+++ frontend/src/pages/MyWord/MyWord.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { FlashcardComponent } from "react-flashcard";
 import { Layout } from "antd";
 
 function MyWord() {
   const { Content } = Layout;
+
+  const cardData = [
+    {
+      front: {
+        text: "living outside, often in a tent",
+        image: "https://o.quizlet.com/RWRdgDus.uuqNDUrJ0ernA.jpg",
+      },
+      back: {
+        text: "Camping",
+      },
+    },
+  ];
 
   return (
     <Layout>
@@ -14,7 +27,7 @@ function MyWord() {
           className="site-layout-background"
           style={{ padding: 24, minHeight: 2400, marginTop: 54 }}
         >
-          Từ của tôi
+          <FlashcardComponent dataSource={cardData} />
         </div>
       </Content>
     </Layout>
