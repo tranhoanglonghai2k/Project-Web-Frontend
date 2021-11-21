2bc5b03ab3c4b4837ca066c95b4a003ad721bb31
tranhoanglonghai2k <tranhoanglonghai2k@gmail.com>
2021-11-21 16:26:29 +0700

    Sign up page

--- frontend/src/pages/Sign up/Signup.js
+++ frontend/src/pages/Sign up/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Layout } from "antd";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 
 function Signup() {
   const { Content } = Layout;
@@ -13,7 +14,9 @@ function Signup() {
         <div
           className="site-layout-background"
           style={{ padding: 24, minHeight: 2400, marginTop: 54 }}
-        ></div>
+        >
+          <RegistrationForm />
+        </div>
       </Content>
     </Layout>
   );
