9df2f9d338194c6c41d2ae5a54aa66acd2923028
tranhoanglonghai2k <tranhoanglonghai2k@gmail.com>
2021-12-15 09:20:11 +0700

    css footer

--- frontend/src/pages/Footer/Footer.js
+++ frontend/src/pages/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Layout, Typography, Space } from "antd";
+import { FacebookFilled, GithubFilled } from "@ant-design/icons";
+import "./Footer.css";
 
 function Footer() {
   const { Footer } = Layout;
@@ -10,9 +12,88 @@ function Footer() {
       <Layout>
         <Footer style={{ textAlign: "center" }}>
           <Space direction="vertical">
-            <Text strong>1. Đào Mạnh Thắng - MSSV: 20184193</Text>
-            <Text strong>2. Trần Thăng Đức - MSSV: 20184076</Text>
-            <Text strong>3. Trần Hoàng Long Hải - MSSV: 20184090</Text>
+            <div className="text-footer">
+              <div className="footer-item mga">
+                <Text strong>
+                  <span
+                    style={{ fontSize: "20px" }}
+                    className="hover-underline-animation"
+                  >
+                    1. Đào Mạnh Thắng - Mssv: 20184193
+                  </span>
+                  <a
+                    href="https://www.facebook.com/BFFLei"
+                    title="Facebook cá nhân"
+                    target="_blank"
+                  >
+                    <FacebookFilled className="icon-default mgl-20" />
+                  </a>
+
+                  <a
+                    href="https://github.com/ThangDao0611"
+                    title="Github cá nhân"
+                    target="_blank"
+                    className="github-link"
+                  >
+                    <GithubFilled className="icon-default mgl-10" />
+                  </a>
+                </Text>
+              </div>
+
+              <div className="footer-item mga">
+                <Text strong>
+                  <span
+                    style={{ fontSize: "20px" }}
+                    className="hover-underline-animation"
+                  >
+                    2. Trần Thăng Đức - Mssv: 20184076
+                  </span>
+                  <a
+                    href="https://www.facebook.com/DucTran2x"
+                    title="Facebook cá nhân"
+                    target="_blank"
+                  >
+                    <FacebookFilled className="icon-default mgl-20" />
+                  </a>
+
+                  <a
+                    href="https://github.com/tranthangduc"
+                    title="Github cá nhân"
+                    target="_blank"
+                    className="github-link"
+                  >
+                    <GithubFilled className="icon-default mgl-10" />
+                  </a>
+                </Text>
+              </div>
+
+              <div className="footer-item mga">
+                <Text strong>
+                  <span
+                    style={{ fontSize: "20px" }}
+                    className="hover-underline-animation"
+                  >
+                    3. Trần Hoàng Long Hải - Mssv: 20184090
+                  </span>
+                  <a
+                    href="https://www.facebook.com/tranhoanglonghai"
+                    title="Facebook cá nhân"
+                    target="_blank"
+                  >
+                    <FacebookFilled className="icon-default mgl-20" />
+                  </a>
+
+                  <a
+                    href="https://github.com/tranhoanglonghai2k"
+                    title="Github cá nhân"
+                    target="_blank"
+                    className="github-link"
+                  >
+                    <GithubFilled className="icon-default mgl-10" />
+                  </a>
+                </Text>
+              </div>
+            </div>
           </Space>
         </Footer>
       </Layout>
