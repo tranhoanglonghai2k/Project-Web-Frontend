2d25cadf1c5ab69bc1e19d4784235c070f63ebd0
tranhoanglonghai2k <tranhoanglonghai2k@gmail.com>
2021-12-21 11:02:25 +0700

    fix Comment

--- frontend/src/components/Comment/Comment.js
+++ frontend/src/components/Comment/Comment.js
@@ -1,67 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { List, Avatar, Skeleton, Divider } from "antd";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React from "react";
+import { Table, Tag, Space } from "antd";
 
-const Comment = () => {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
+const columns = [
+  {
+    title: "Word",
+    dataIndex: "word",
+    key: "word",
+  },
+  {
+    title: "Spell",
+    dataIndex: "spell",
+    key: "spell",
+  },
+  {
+    title: "Example",
+    dataIndex: "example",
+    key: "example",
+  },
 
-  const loadMoreData = () => {
-    if (loading) {
-      return;
-    }
-    setLoading(true);
-    fetch(
-      "https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo"
-    )
-      .then((res) => res.json())
-      .then((body) => {
-        setData([...data, ...body.results]);
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  };
+  {
+    title: "Mean of Example",
+    dataIndex: "meanofexample",
+    key: "meanofexample",
+  },
+];
 
-  useEffect(() => {
-    loadMoreData();
-  }, []);
+const data = [
+  {
+    key: "1",
+    word: "John Brown",
+    spell: 32,
+    example: "New York No. 1 Lake Park",
+    meanofexample: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    word: "Jim Green",
+    spell: 42,
+    example: "London No. 1 Lake Park",
+    meanofexample: ["loser"],
+  },
+  {
+    key: "3",
+    word: "Joe Black",
+    spell: 32,
+    example: "Sidney No. 1 Lake Park",
+    meanofexample: ["cool", "teacher"],
+  },
+];
 
+function Comment() {
   return (
-    <div
-      id="scrollableDiv"
-      style={{
-        height: 400,
-        overflow: "auto",
-        padding: "0 16px",
-        border: "1px solid rgba(140, 140, 140, 0.35)",
-      }}
-    >
-      <InfiniteScroll
-        dataLength={data.length}
-        next={loadMoreData}
-        hasMore={data.length < 50}
-        loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
-        endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-        scrollableTarget="scrollableDiv"
-      >
-        <List
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item key={item.id}>
-              <List.Item.Meta
-                avatar={<Avatar src={item.picture.large} />}
-                title={<a href="https://ant.design">{item.name.last}</a>}
-                description={item.email}
-              />
-              <div>Content</div>
-            </List.Item>
-          )}
-        />
-      </InfiniteScroll>
+    <div>
+      <Table columns={columns} dataSource={data} />
     </div>
   );
-};
+}
 
 export default Comment;
